1684d5213e62fe35b34e22c482e846ad4e542390
EderCruz <>
2021-10-21 19:14:42 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+/**
+* @author Eder Cruz
+*/
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("La suma es: "+suma(3,3));
+  }
+
+  /**
+  * @param a Primer entero de la suma
+  * @param b Segundo entero de la suma
+  * @return La suma de los enteros a más b
+  */
+  public static int suma(int a, int b){
+    return a+b;
+  }
+}
